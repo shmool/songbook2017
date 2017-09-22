2f9489b8e4afed3095444a3a799398622299e351
Shmuela <shmuela@500tech.com>
2017-09-22 22:13:17 +0200

    add firebase tools

---
+++ functions/index.js
@@ -0,0 +1,19 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+function addUid(event) {
+  if (event.auth.admin) return;
+
+  event.data.adminRef.update({uid: event.auth.variable.uid});
+}
+
+exports.saveSong = functions.database.ref('/songs/{id}')
+  .onWrite(event => {
+    addUid(event);
+  });
